3df66154fd9d6dcfe4c7e81598c07f9eb1849447
Ros <rosicela.pinedo.quespi@gmail.com>
2024-08-14 08:14:42 -0400

    14-08-24

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "bd_gimnasio");
+$con = new mysqli("localhost", "root", "", "bd_gimnasio");
 if ($con->connect_error)
     die("conexion fallada" . $con->connect_error);
 ?>
